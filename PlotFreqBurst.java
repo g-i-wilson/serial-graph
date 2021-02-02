e8a0f55924cf87e971d0d43160f4b587a21b4458
Gabriel Wilson <47883480+g-i-wilson@users.noreply.github.com>
2021-02-02 15:10:45 -0600

    graphs are looking like they may be usable...

--- PlotFreqBurst.java
+++ PlotFreqBurst.java
@@ -6,6 +6,15 @@ import java.awt.Color;
 
 public class PlotFreqBurst {
 
+  private GraphModelShapes phaseModel;
+  private GraphModelShapes freqModel;
+  private GraphModelShapes rfModel;
+  private GraphView phaseWindow;
+  private GraphView freqWindow;
+  private GraphView rfWindow;
+	private Socket socket;
+
+
 	public static double multReal (double aR, double aI, double bR, double bI) {
 		return aR*bR - aI*bI;
 	}
@@ -13,41 +22,31 @@ public class PlotFreqBurst {
 	public static double multImag (double aR, double aI, double bR, double bI) {
 		return aR*bI + aI*bR;
 	}
+	
 
-  public static void main (String[] args) throws Exception {
+	public PlotFreqBurst () throws Exception {
+		phaseModel = new GraphModelShapes(200.0, 200.0, -200.0, -200.0, new Color( 0, 127, 255 ));
+		freqModel = new GraphModelShapes(200.0, 200.0, -200.0, -200.0, new Color( 0, 127, 255 ));
+		rfModel = new GraphModelShapes(200.0, 200.0, -200.0, -200.0, new Color( 0, 127, 255 ));
 
-    GraphModelShapes phaseModel = new GraphModelShapes(50.0, 50.0, -50.0, -50.0, new Color( 0, 127, 255 ));
-    GraphModelShapes freqModel = new GraphModelShapes(50.0, 50.0, -50.0, -50.0, new Color( 0, 127, 255 ));
+		phaseWindow = new GraphViewGUI( phaseModel, 600, 600, new Color( 0, 255, 0 ), "Phase" );
+		freqWindow = new GraphViewGUI( freqModel, 600, 600, new Color( 255, 0, 255 ), "Frequency" );
+		rfWindow = new GraphViewGUI( rfModel, 600, 600, new Color( 0, 255, 255 ), "RF DIV" );
 
-    GraphView phaseWindow = new GraphViewGUI( phaseModel, 1200, 400, new Color( 0, 255, 0 ), "Phase" );
-    GraphView freqWindow = new GraphViewGUI( freqModel, 1200, 400, new Color( 255, 0, 255 ), "Frequency" );
+		socket = new Socket( "localhost", 9090 );
+	}
 
-		Socket socket = new Socket( "localhost", 9090 );
-
-		while(true) {
-		
-//			InputStream in = new ByteArrayInputStream(
-//				new byte[]{
-//					'r','x',0x00,(byte)0x80,0x00,(byte)0x80,0x01,0x00,0x00,0x00,(byte)0xeb,
-//					'r','x',0x01,0x00,0x01,0x00,0x01,0x00,0x00,0x01,(byte)0xee,
-//					'r','x',0x02,0x00,0x02,0x00,0x01,0x00,0x00,0x02,(byte)0xf1,
-//					'r','x',0x04,0x00,0x04,0x00,0x01,0x00,0x00,0x03,(byte)0xf6,
-//					'r','x',0x08,0x00,0x08,0x00,0x01,0x00,0x00,0x04,(byte)0xff,
-//					'r','x',0x10,0x00,0x10,0x00,0x01,0x00,0x00,0x05,(byte)0x10
-//				}
-//			);
-			
-			//Thread.sleep(2000);
+	public void plotGraph ( int rampStart, int rampEnd, int preSamples, int stepSize, int postSamples) throws Exception {
 
 			FreqBurst aBurst = new FreqBurst(
 				socket.getInputStream(),
 				new PrintStream(socket.getOutputStream()),
-				15, // ramp start
-				0, // ramp end
+				rampStart, // ramp start
+				rampEnd, // ramp end
 				0, // cycles-1
-				10, // pre samples
-				5, // step samples
-				300 // post samples
+				preSamples, // pre samples
+				stepSize, // step samples
+				postSamples // post samples
 			);
 			
 
@@ -57,9 +56,16 @@ public class PlotFreqBurst {
 		  double iValPrev = 0;
 		  double qValPrev = 0;
 		  double divVal = 0;
+		  double fVal = 0;
+		  double pVal = 0;
 		  
 		  SortedMap<Integer,SortedMap<Integer,Map<String,Integer>>> rx = aBurst.rxData();
 		  
+			//Thread.sleep(500);
+	  	phaseModel.clear();
+	  	freqModel.clear();
+	  	rfModel.clear();
+
 		  for (SortedMap<Integer,Map<String,Integer>> cycle : rx.values()) {
 		  	for (Map<String,Integer> sample : cycle.values()) {
 					x++;
@@ -69,28 +75,56 @@ public class PlotFreqBurst {
 				  iVal = (double) sample.get("I").intValue();
 				  qVal = (double) sample.get("Q").intValue();
 				  divVal = (double) sample.get("RFDIV").intValue();
+				  //phase
+				  pVal = Math.atan2(iVal, qVal);
+				  // frequency
+				  //fVal = Math.log(
+				  //	Math.atan2(
+					//		multReal(iVal, qVal, iValPrev, -qValPrev),
+					//		multImag(iVal, qVal, iValPrev, -qValPrev)
+				  //	)
+				  //)*100;
+				  fVal = Math.atan2(
+							multReal(iVal, qVal, iValPrev, -qValPrev),
+							multImag(iVal, qVal, iValPrev, -qValPrev)
+				  	);
 
 				  //phaseModel.plotPoint( x, iVal/100 );
 				  //freqModel.plotPoint( x, qVal/100 );
-				  phaseModel.plotPoint( x, Math.atan2(iVal, qVal)*10 );
-				  freqModel.plotPoint( x, Math.atan2(
-				  	multReal(iVal, qVal, iValPrev, -qValPrev),
-				  	multImag(iVal, qVal, iValPrev, -qValPrev)
-				  )*10 );
+				  phaseModel.plotPoint( x-180, pVal*50 );
+				  freqModel.plotPoint( x-180, fVal*100 );
+				  rfModel.plotPoint( x, divVal*3 );
 				  
 				  //System.out.println("I: "+iVal+", Q: "+qVal+", RFDIV: "+divVal);
+				  //System.out.println(x+", "+fVal);
 
-				  phaseWindow.refresh();
-				  freqWindow.refresh();
+				  //phaseWindow.refresh();
+				  //freqWindow.refresh();
+				  //rfWindow.refresh();
 				  
 				  //Thread.sleep(100);
+				  //System.out.println(x);
 				  
 				}
 			}
-			
-			//Thread.sleep(500);
-	  	phaseModel.clear();
-	  	freqModel.clear();
+		  phaseWindow.refresh();
+		  freqWindow.refresh();
+		  rfWindow.refresh();
+	  	
+	}
+
+  public static void main (String[] args) throws Exception {
+  
+  	PlotFreqBurst pfb = new PlotFreqBurst();
+
+		while(true) {
+		
+		  //Thread.sleep(1000);
+		  //System.out.println("up");
+			pfb.plotGraph( 0, 15, 0, 5, 300 );
+		  //Thread.sleep(1000);
+		  //System.out.println("down");
+			//pfb.plotGraph( 9, 8, 0, 5, 0 );
 
 		}
   }
