4560d50fdaf0304df42257cf4aa42b23d4f2654b
Gabriel Wilson <47883480+g-i-wilson@users.noreply.github.com>
2021-03-04 17:00:53 -0600

    updates to TestMemoryMapServer

--- TestMemoryMapServer.java
+++ TestMemoryMapServer.java
@@ -2,54 +2,47 @@ import java.io.*;
 import java.net.*;
 
 public class TestMemoryMapServer {
-  public static void main (String[] args) throws Exception {
 
+	public static void write (PrintStream output, int addrHigh, int addrLow, int data) throws Exception {
 		Packet writeData = new Packet(
 			new int[]{ 'm','0' },
-			new int[]{ 0, 4, 7 }
+			new int[]{ addrHigh, addrLow, data }
 		);
-		System.out.println( writeData );
-		
-		Packet readData = new Packet(
-			new int[]{ 'm','0' },
-			new int[]{ 128, 4, 0 }
-		);
-		System.out.println( readData );
-		
+		for (Integer val : writeData.packet()) {
+			output.write(val);
+		}
+		output.write('\n'); // in case the output interface requires a return; may be optional		
+	}
+	
+	public static int read (PrintStream output, int addrHigh, int addrLow, InputStream input) throws Exception {
+		write( output, addrHigh & 0x80, addrLow, 0 );
 		Packet receiveData = new Packet(
 			new int[]{ 'm','0' },
 			3
 		);
-		System.out.println( receiveData );
-		
+		while(true) {
+		  int b = input.read();
+		  if (b != -1) {
+		  	System.out.println( b );
+		  	receiveData.add(b);
+		  	if (receiveData.valid()) return receiveData.data8(5);
+		  }
+		}
+	}
+
+
+  public static void main (String[] args) throws Exception {
 		
 		Socket socket = new Socket( "localhost", 9090 );
-		InputStream input = socket.getInputStream();
-		PrintStream output = new PrintStream(socket.getOutputStream());
-
-		for (Integer val : writeData.packet()) {
-			output.write(val);
-		}
-		output.write('\n'); // in case the output interface requires a return; may be optional
-
-		for (Integer val : readData.packet()) {
-			output.write(val);
-		}
-		output.write('\n'); // in case the output interface requires a return; may be optional
-
-
-		System.out.println("here!");
 		
-	  while(true) {
-	    int b = input.read();
-	    if (b != -1) {
-	    	System.out.println( b );
-	    	receiveData.add(b);
-	    	if (receiveData.valid()) break;
-	    }
-	  }
-	  
-	  System.out.println( receiveData );
+		InputStream in = socket.getInputStream();
+		PrintStream out = new PrintStream(socket.getOutputStream());
+
+		write( out, 0, 2,   5 );
+		write( out, 0, 3,   7 );
+		System.out.println(
+			read ( out, 0, 2, in )
+		);
 	  
 	}
 }
